bdfee66d005276e5fc35d3be666f2703b027ea0f
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2023-02-16 11:53:30 -0300

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
+const { useState } = require("react");
+
 const UserTasks = ({
     user,
     addLoading,
@@ -10,5 +12,11 @@ const UserTasks = ({
     onUpdate
 }) => {
     const newTodo = useInputValue('');
-    
+
+    const handleAddTodo = () => (
+        //clear txt input
+        newTodo.onChange(''),
+        //send
+        onAdd(newTodo.value)
+    )
 }
