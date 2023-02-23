cac0e94fbef4421c0deef9866c4f2aafdf278ccf
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2023-02-23 15:34:51 -0300

    ++

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -14,8 +14,9 @@ function classNames(...classes) {
 export default function searchField() {
   const [query, setQuery] = useState('')
 
-  const [open, setOpen] = useState(true)
-
+  //usar lodash no filter
+  //scroll
+  //limitar dados
   const filteredPeople =
     query === ''
       ? []
@@ -24,31 +25,7 @@ export default function searchField() {
         })
 
   return (
-    <Transition.Root show={open} as={Fragment} afterLeave={() => setQuery('')} appear>
-      <Dialog as="div" className="relative z-10" onClose={setOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <Dialog.Panel className="mx-auto max-w-xl transform rounded-xl bg-white p-2 shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
+    
               <Combobox onChange={(person) => (window.location = person.url)}>
                 <Combobox.Input
                   className="w-full rounded-md border-0 bg-gray-100 px-4 py-2.5 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
@@ -85,10 +62,6 @@ export default function searchField() {
                   </div>
                 )}
               </Combobox>
-            </Dialog.Panel>
-          </Transition.Child>
-        </div>
-      </Dialog>
-    </Transition.Root>
+
   )
 }
